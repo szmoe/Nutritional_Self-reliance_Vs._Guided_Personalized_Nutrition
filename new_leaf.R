2374a1a409ab396c81c8a1a411f22d11802e5f56
szmoe <125073552+szmoe@users.noreply.github.com>
2024-03-28 17:44:16 +0630

    start

---
+++ new_leaf.R
@@ -0,0 +1,18 @@
+# Try the model again after Monte Carlo Chapter
+# The first attempt was very frequentist- I was trying to simulate the truth as 
+# best as possible 
+# Based on DA course, process needs to be:
+# 1. Simple and not time-consuming (not data-driven; try to make decision even when data is scarce)
+# 2. Start simple and add on complexity
+# 3. Collect data from any possible sources- transdisplinary
+
+# Based on statistical rethinking, bayesian process is similar to frequentist in base theory
+# So, the basic principles still apply- I can't go to town with the model just coz the decisionSupport
+# package allows 
+# Problem with overfitting with complex models- meaning here in DA model, unnecessary complexity brings in chaos!
+
+
+## Simulation is for a small homogenous group- so less generalization)
+## I may be able to treat it as client level- not population level
+
+# Do base/ standard first
