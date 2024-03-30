be93031e4f1971c07b8c27795df14bf6dd6b28a2
szmoe <125073552+szmoe@users.noreply.github.com>
2024-03-30 10:04:55 +0630

    test connection

--- new_leaf.R
+++ new_leaf.R
@@ -11,8 +11,38 @@
 # package allows 
 # Problem with overfitting with complex models- meaning here in DA model, unnecessary complexity brings in chaos!
 
+## In order to build a good model, I think I need to learn more on:
+
+## Bayesian Statistics: 
+# I initially thought Bayesian statistics is something new. I mistook how the
+# decisionSupport package works as Bayesian statistics- no rules and lots of freedom and I got 
+# super excited (since frequentist approach has lots of rules) and got carried away in my first try. 
+# Now I'm reading rethinking, I understood Bayesian is something different, not something totally new.
+# So, I need to be a good statistician to build a good model. Being a nutritionist doesn't matter anymore.
+# Nutrition knowledge is pretty much straightforward (anyone can learn it), but I need to think like a
+# good statistician to build a good nutrition model. A nutritionist without good statistic knowledge 
+# would do senseless causal correlations (esp when decisionSupport allows estimate range for uncertainty),
+# and overestimate the outcome. And most (if not all) nutrition studies report positive results. 
+# A good statistician would see limitations and confounds, and know how to properly use the finding without
+# being overconfident about the results. 
+
+## Economics:
+# I need to learn how to assess economic values of intangible things. I plan to read "how to 
+# measure anything" but still at first chapter. Also, I need to look up more resources on how
+# economists assign values to health outcomes. 
+
+##########################################################################################################
 
 ## Simulation is for a small homogenous group- so less generalization)
 ## I may be able to treat it as client level- not population level
 
 # Do base/ standard first
+
+
+
+
+
+
+
+
+
