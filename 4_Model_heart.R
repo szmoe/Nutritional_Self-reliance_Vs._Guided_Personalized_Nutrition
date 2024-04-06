ed99d2e8ba7ea598733664786e4398b8b6979b5e
szmoe <125073552+szmoe@users.noreply.github.com>
2024-04-06 20:40:04 +0630

    dilemma

--- 4_Model_heart.R
+++ 4_Model_heart.R
@@ -14,3 +14,28 @@
 ## To draw impact pathway model of chosen parameters and possible confounders
 
 # time and recovery
+
+## How to gather parameters/predictors for impact pathway model here?
+# 1. Use logical thinking
+# 2. Literature review
+# 3. Focus group discussion
+
+# It would be hard to predict good/bad predictors and their correlations even with literature review-
+# due to differences in population and circumstances, etc.
+
+# A real regression analysis method may not be suitable here but how can I use that logic when all I have are priors? 
+# or can estimates be likelihood and I draw samples??
+
+# Remember I'm not predicting the future outcome accurately, but simulating the most possible outcome given the 
+# current circumstances. So, the result is not the future but a good predictor of the future?? 
+# If I use all predictors gathered from focus group discussion and giving estimates to those, 
+# I'm assuming causal relationships between each variable in input table and the outcome variable.
+# And all those relationships give either positive or negative effect on outcome variables. 
+# And, everything is more or less normal distribution. I'm not considering the effects of predictors on each other
+# and those predictors might not be a good predictors for the targeted outcome. Basically, I am assuming all those
+# predictor are perfectly independent of each other and have established causal relationship with the outcome, giving
+# intercept values as ranges of estimates. 
+# Need to vet those predictors- how to do that without observed data?? think think think
+
+
+
