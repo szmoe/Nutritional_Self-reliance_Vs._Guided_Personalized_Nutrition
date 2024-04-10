d4fefa235db2dd508c1e01b6e3e8b9e2cc065b84
szmoe <125073552+szmoe@users.noreply.github.com>
2024-04-10 09:49:43 +0630

    n

--- 4_Model_heart.R
+++ 4_Model_heart.R
@@ -1,41 +1,19 @@
 
 ### Model 4: Chance of Winning
 
-# This is the heart of the model where statisticians' inputs will be crucial.
-# The best scenario would be to have a group of experts with good statistic knowledge
-# Experts here include decision analysts, management, football coaches, football players,
-# medical staff, and nutritionists
+# The question is "can improved nutrition result in higher chance of winning a football game?" 
+# Here, higher chance does not mean actually winning the game, so this is a logical expectation: 
+# sports performance improves with good nutrition and that can increase chance of winning to 
+# some extent. But, this is a linear assumption: every action leads to desired outcome. 
+# In the complexity of life, this assumption can be seriously flawed- a number of factors may override our action 
+# and gives an unrelated outcome without improving our chance at all. A regression analysis can take some of these
+# factors into consideration but the model is trying to predict future with little data. 
 
-# Decision analyst will surely have good statistic knowledge (very much expected)
-# Medical staff and nutritionists may have if they are well-trained in research (at least will hv basic stat knowledge)
-# Management, coaches and athletes may not have statistic knowledge but they have experience- 
-   # they will be able to identify good parameters and confounders 
-
-## To draw impact pathway model of chosen parameters and possible confounders
-
-# time and recovery
-
-## How to gather parameters/predictors for impact pathway model here?
-# 1. Use logical thinking
-# 2. Literature review
-# 3. Focus group discussion
-
-# It would be hard to predict good/bad predictors and their correlations even with literature review-
-# due to differences in population and circumstances, etc.
-
-# A real regression analysis method may not be suitable here but how can I use that logic when all I have are priors? 
-# or can estimates be likelihood and I draw samples??
-
-# Remember I'm not predicting the future outcome accurately, but simulating the most possible outcome given the 
-# current circumstances. So, the result is not the future but a good predictor of the future?? 
-# If I use all predictors gathered from focus group discussion and giving estimates to those, 
-# I'm assuming causal relationships between each variable in input table and the outcome variable.
-# And all those relationships give either positive or negative effect on outcome variables. 
-# And, everything is more or less normal distribution. I'm not considering the effects of predictors on each other
-# and those predictors might not be a good predictors for the targeted outcome. Basically, I am assuming all those
-# predictor are perfectly independent of each other and have established causal relationship with the outcome, giving
-# intercept values as ranges of estimates. 
-# Need to vet those predictors- how to do that without observed data?? think think think
-
-
-
+# So, I should treat this model as a weather forecast (the example given in the stat book). Weather forecasts are
+# based on previous weather patterns and it can be wrong when an unexpected factor comes to play? But they can be 
+# very accurate in short-term predictions, etc. That means i don't need to wreck my brain to find good predictors
+# for my model or create samples, but need to draw out obvious patterns. The model is progressive, so it
+# will become a stronger decision support tool as more data becomes available or current patterns are enough. 
+# I think I get it. Various experts will give various inputs, and I need to select patterns and input with high
+# relevancy (also some historical data if possible). Build initial trends with it and refine the model as
+# I collect more data. 
